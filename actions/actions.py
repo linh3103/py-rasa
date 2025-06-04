c5d2aec65f66eb1f5f25c629bb9c254f1098ce87
unknown <quanglinh3103@gmail.com>
2025-06-04 16:58:03 +0700

    update training data

--- actions/actions.py
+++ actions/actions.py
@@ -13,16 +13,30 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from requests import Response
 
-class ActionHelloWorld(Action):
+class ActionConfirmOrder(Action):
 
     def name(self) -> Text:
-        print("ActionHelloWorld called")
-        return "action_hello_world"
+        return "action_confirm_order"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print("ActionHelloWorld run method called")
-        dispatcher.utter_message(json_message={"message": "123"})
+        
+        product = tracker.get_slot("product_name")
+        count = tracker.get_slot("count")
+        size = tracker.get_slot("size")
+        color = tracker.get_slot("color")
+        user_name = tracker.get_slot("user_name")
+        address = tracker.get_slot("address")
+        phone_number = tracker.get_slot("phone_number")
 
+        order_info = f"Vui lòng xác nhận đơn hàng:\n"
+        order_info += f"Tên khách hàng: {user_name}\n"
+        order_info += f"Sản phẩm: {product}, size: {size}, màu: {color}\n"
+        order_info += f"Số lượng: {count}\n"
+        order_info += f"Địa chỉ giao hàng: {address}\n"
+        order_info += f"Số điện thoại: {phone_number}"
+
+        dispatcher.utter_message(text=order_info)
+        
         return []
